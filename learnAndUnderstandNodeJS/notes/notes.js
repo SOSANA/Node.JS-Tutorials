1eb714e5c036d180c3e82737991c9ebc3cc2e4e2
Zach Sosana <thesosana@gmail.com>
2015-10-07 15:27:36 +0000

    updating

---
+++ learnAndUnderstandNodeJS/notes/notes.js
@@ -0,0 +1,37 @@
+/**
+ * A JavaScript Engine:
+ *  - a program that converts javascript code into something the computer
+ *    processor can understand
+ *  - follows the ECMAScript standard on how the language should work and 
+ *    what features it should have
+ *  - ex: 
+ *      - V8 (google), SpiderMonkey (Mozilla | MDN), and many more
+ *      - ref: https://en.wikipedia.org/wiki/List_of_ECMAScript_engines
+ * 
+ * Arguments:
+ *  - values you give your program that affect how it runs
+ *  - essentially passing parameters to a function
+ * 
+ * BreakPoint:
+ *  - a spot in our code where we tell a debugging tool to pause the execution
+ *    of our code so we can figure out what's going on
+ * 
+ * ECMAScript:
+ *  - the standard javascript is based on
+ *  - needed a standard since there are many engines
+ * 
+ * Command Line Interface (cli):
+ *  - a utility to type commands to your computer rather than click
+ *  - ex:
+ *    - bash on linux
+ *    - terminal on mac
+ *    - command prompts on windows
+ *  - ref: http://cli.learncodethehardway.org/book/
+ * 
+ * Machine Code (Language):
+ *  - programming languages spoken by computer processors
+ *  - every program you run on your computer has been converted (compiled)
+ *    into machine code
+ *  - Node and V8 are written in C++, and is closer to the machine code
+ *  - ref: notes/levelOfAbstraction.png
+ */
