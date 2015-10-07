230a36faa15473dc12aca013b723a673d886242d
sosana <thesosana@gmail.com>
2015-10-07 11:32:47 -0600

    creating examples of first-class functions, functions on the fly, function statement, and function expressions

--- learnAndUnderstandNodeJS/section4/D2_FirstClassFunctions/Starter/app.js
+++ learnAndUnderstandNodeJS/section4/D2_FirstClassFunctions/Starter/app.js
@@ -1,1 +1,25 @@
-// Your Javascript Code Goes Here
+// function statement
+function greet() {
+  console.log('hi');
+}
+greet();
+
+// functions are First-Class
+function logGreeting(fn) {
+  fn();
+}
+logGreeting(greet);
+
+// function Expression
+var greetMe = function() {
+  console.log('Hi Sosana');
+};
+greetMe();
+
+// its First-Class
+logGreeting(greetMe);
+
+// use a function expression on the fly
+logGreeting(function() {
+  console.log('Hello Sosana');
+});
