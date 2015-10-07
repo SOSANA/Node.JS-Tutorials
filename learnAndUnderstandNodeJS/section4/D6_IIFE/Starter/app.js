efafa1e7f41f84f21c2875938c242b4c7b60b3f4
sosana <thesosana@gmail.com>
2015-10-07 13:13:53 -0600

    creating an example of scope with IIFEs

--- learnAndUnderstandNodeJS/section4/D6_IIFE/Starter/app.js
+++ learnAndUnderstandNodeJS/section4/D6_IIFE/Starter/app.js
@@ -1,1 +1,11 @@
-// Your Javascript Code Goes Here
+var firstname = 'jane';
+
+// most common way to use IIFEs (Immediately Invoked function Expressions)
+(function (lastname) {
+
+  var firstname = 'John';
+  console.log(firstname);
+  console.log(lastname);
+}('Doe'));
+
+console.log(firstname);
