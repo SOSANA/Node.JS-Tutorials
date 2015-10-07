f47daf81f5baa5f0f327043cae1b76247a21fd4a
sosana <thesosana@gmail.com>
2015-10-07 11:31:35 -0600

    updating notes on first class functions and function expressions

--- learnAndUnderstandNodeJS/notes/notes.js
+++ learnAndUnderstandNodeJS/notes/notes.js
@@ -2,24 +2,22 @@
  * A JavaScript Engine:
  *  - a program that converts javascript code into something the computer
  *    processor can understand
- *  - follows the ECMAScript standard on how the language should work and 
+ *  - follows the ECMAScript standard on how the language should work and
  *    what features it should have
- *  - ex: 
+ *  - ex:
  *      - V8 (google), SpiderMonkey (Mozilla | MDN), and many more
  *      - ref: https://en.wikipedia.org/wiki/List_of_ECMAScript_engines
- * 
+ *
  * Arguments:
  *  - values you give your program that affect how it runs
  *  - essentially passing parameters to a function
- * 
+ *
  * BreakPoint:
  *  - a spot in our code where we tell a debugging tool to pause the execution
  *    of our code so we can figure out what's going on
- * 
- * ECMAScript:
- *  - the standard javascript is based on
- *  - needed a standard since there are many engines
- * 
+ *  - ref: section4/D2_FirstClassFunctions & watch lecture 17
+ *  	https://www.udemy.com/understand-nodejs/learn/#/lecture/3484570
+ *
  * Command Line Interface (cli):
  *  - a utility to type commands to your computer rather than click
  *  - ex:
@@ -27,11 +25,41 @@
  *    - terminal on mac
  *    - command prompts on windows
  *  - ref: http://cli.learncodethehardway.org/book/
- * 
+ *
+ * CommonJS Modules:
+ * 	- an agreed upon standard for how code modules should be structured
+ *
+ * ECMAScript:
+ *  - the standard javascript is based on
+ *  - needed a standard since there are many engines
+ *
+ * First-Class Functions:
+ * 	- everything you can do with other types (strings, numbers, etc) you can
+ * 		do with functions.
+ * 	- you can use functions like strings, numbers, etc.
+ * 	- ex:
+ * 			- pass them around
+ * 			- set variables equal to them
+ * 			- put them in arrays and more
+ * 	- ref: section4/D2_FirstClassFunctions
+ *
+ * Function Expressions:
+ * 	- a block of code that results in a values
+ * 	- are possible in javascript because functions are First-Class.
+ * 	- ref: section4/D2_FirstClassFunctions
+ *
+ * Invoke:
+ * 	- run the function
+ * 	- we can also say 'call' the function
+ *
  * Machine Code (Language):
  *  - programming languages spoken by computer processors
  *  - every program you run on your computer has been converted (compiled)
  *    into machine code
  *  - Node and V8 are written in C++, and is closer to the machine code
  *  - ref: notes/levelOfAbstraction.png
+ *
+ * Module:
+ * 	- a reusable block of code whose existence does not accidentally impact
+ * 		other code. JavaScript didn't have this before
  */
