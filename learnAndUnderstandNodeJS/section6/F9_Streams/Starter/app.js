22e3d9628f1e71734325eb19aa63972f882a76bf
sosana <thesosana@gmail.com>
2015-10-12 17:32:33 -0600

    created an example of working with fs, reading and writing files

--- learnAndUnderstandNodeJS/section6/F9_Streams/Starter/app.js
+++ learnAndUnderstandNodeJS/section6/F9_Streams/Starter/app.js
@@ -1,9 +1,15 @@
+// remember that buffers only hold a certain amount of data in 'chunks'
 var fs = require('fs');
-
+// we are able to use 'highWaterMark' to break that data into chunks
+// creating a readable stream
 var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
-var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
-
-readable.on('data', function(chunk) {
-	console.log(chunk.length);
-	writable.write(chunk);
+// creating a writeable stream
+var writeable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+// adding a listener by listening to 'data' and sending the data to a writeable stream
+// a piece at a time with 'highWaterMark' and we didn't need a massive buffer and sent
+// it in pieces and minimize the amount of memory our app is using
+readable.on('data', function (chunk) {
+	console.log(chunk);
+	// coping greet.txt and writing it to new file greetcopy.txt
+	writeable.write(chunk);
 });
