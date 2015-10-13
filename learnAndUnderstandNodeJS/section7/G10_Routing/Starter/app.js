d3fe975754e157c102250fb7038493591c2e7566
sosana <thesosana@gmail.com>
2015-10-13 16:07:44 -0600

    working with node core routing

--- learnAndUnderstandNodeJS/section7/G10_Routing/Starter/app.js
+++ learnAndUnderstandNodeJS/section7/G10_Routing/Starter/app.js
@@ -3,11 +3,22 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
 
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  var obj = {
-    firstname: 'John',
-    lastname: 'Doe'
-  };
-  res.end(JSON.stringify(obj));
+  if (req.url === '/') {
+    fs.createReadStream(__dirname + '/index.htm').pipe(res);
+  }
+  else if (req.url === '/api') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var obj = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
+  }
+  else{
+    res.writeHead(404);
+    res.end();
+  }
 
-}).listen(8080, '127.0.0.1');
+}).listen(1337, '127.0.0.1');
+
+console.log('Listening on localhost:1337');
