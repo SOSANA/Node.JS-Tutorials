e653c4b027c5747f72d2071fa44a818c4f3a3b22
sosana <thesosana@gmail.com>
2015-10-14 14:42:52 -0600

    updated with body-parser, post parameters, and jsonParser

--- learnAndUnderstandNodeJS/section9/nodeTestApp/app.js
+++ learnAndUnderstandNodeJS/section9/nodeTestApp/app.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// create application/json parser
+var jsonParser = bodyParser.json();
 
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -15,9 +21,21 @@ app.use('/', function (req, res, next) {
 app.get('/', function (req, res) {
   res.render('index');
 });
-// colon '/:id' tells express it could be anything
+
 app.get('/person/:id', function (req, res) {
-  res.render('person', { ID: req.params.id });
+  res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
+});
+
+app.post('/person', urlencodedParser, function (req, res) {
+  res.send('Thank you!!');
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function (req, res) {
+  res.send('Thank you for the JSON data!');
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
 });
 
 app.get('/api', function (req, res) {
