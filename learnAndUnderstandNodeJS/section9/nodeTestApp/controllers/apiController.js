23d290853f0bdfa80837273abd687a8439eb166d
sosana <thesosana@gmail.com>
2015-10-14 15:30:48 -0600

    created new folder for refactor

---
+++ learnAndUnderstandNodeJS/section9/nodeTestApp/controllers/apiController.js
@@ -0,0 +1,15 @@
+module.exports = function (app) {
+  app.get('/api/person/:id', function (req, res) {
+    // get the data from database
+    res.json({ firstname: 'John', lastname: 'Doe' });
+  });
+
+  app.post('/api/person', function (req, res) {
+    // save to the database
+  });
+
+  app.delete('/api/person/:id', function (req, res) {
+    // delete from the database
+
+  });
+};
