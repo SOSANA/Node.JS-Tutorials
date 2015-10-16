41a8de161b75cf0fab00e52eda8eeba68d8cfb93
sosana <thesosana@gmail.com>
2015-10-16 16:02:41 -0600

    updating notes and example on using module.exports, require, and exports

--- nodeinpractice/node-techniques/ch2/2/index.js
+++ nodeinpractice/node-techniques/ch2/2/index.js
@@ -4,12 +4,20 @@
  * Solution:
  * Use the exports object
  *
- * shows how to load these modules with requre, and how to use the functionality
+ * shows how to load these modules with require, and how to use the functionality
  * they provide
+ *
+ * Which Module?
+ * to determine the exact module Node will load use 'require.resolve' (id). This
+ * will return a fully expanded filename
+ * ex:
+ * var myClass = require.resolve('./myclass');
+ * var module2 = require.resolve('./module-2');
+ * console.log(myClass); // C:\dev\Node.JS-Tutorials\nodeinpractice\node-techniques\ch2\2\myclass.js
+ * console.log(module2); // C:\dev\Node.JS-Tutorials\nodeinpractice\node-techniques\ch2\2\module-2.js
  */
 var myClass = require('./myclass');
 var module2 = require('./module-2');
-
 
 console.log(myClass.method());
 console.log(module2.method());
