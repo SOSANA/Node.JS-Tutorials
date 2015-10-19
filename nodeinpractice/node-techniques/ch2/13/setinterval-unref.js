820a35a240d302a459a844174e706e284c0cc346
sosana <thesosana@gmail.com>
2015-10-19 16:11:18 -0600

    running callbacks periodically with timers, using setInterval() and .unref()

--- nodeinpractice/node-techniques/ch2/13/setinterval-unref.js
+++ nodeinpractice/node-techniques/ch2/13/setinterval-unref.js
@@ -1,9 +1,16 @@
+/**
+ * 	- keeping a timer alive until the program cleanly exits
+ * 	- extremely useful in situations where there isn't a good place to
+ * 		call 'clearInterval()';
+ */
 function monitor() {
   console.log(process.memoryUsage());
 }
 
 var id = setInterval(monitor, 1000);
-id.unref(); //<co id="callout-globals-setinterval-unref" />
+// tell node to stop the interval when the program has finished
+// the long running operation
+id.unref();
 
 setTimeout(function() {
   console.log('Done!');
--- nodeinpractice/node-techniques/ch2/13/setinterval.js
+++ nodeinpractice/node-techniques/ch2/13/setinterval.js
@@ -1,11 +1,16 @@
 /**
- * Technique 13: 
- * 	-
+ * Technique 13: Running callbacks periodically with timers
+ * 	- node can also run callbacks at regular intervals using 'setInterval()',
+ * 		which works in a fashion similar to setTimeout()
  *
  * Problem:
- * 	-
+ * 	- you want to run a callback at regular interval
  * Solution:
- * 	-
+ * 	- use 'setInterval()', and 'clearInterval()' to stop the timer
+ *
+ * 	- the callback will be executed on or just after the specified delay, and
+ * 		will run in the event loop just after I/O (and any calls to setImmedate(),
+ * 		as detailed in technique 14)
  */
 function tick() {
   console.log('tick:', Date.now());
@@ -17,6 +22,7 @@ function tock() {
 
 setInterval(tick, 1000);
 
-setTimeout(function() { //<co id="callout-globals-setinterval" />
+setTimeout(function() {
+  // run another 'setInverval()' after the first one
   setInterval(tock, 1000);
 }, 500);
