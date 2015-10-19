ba39261b20ea703a662424501ef0cd4705ac3033
sosana <thesosana@gmail.com>
2015-10-19 12:15:25 -0600

    updating notes on file/dir path, creating, managing andloading modules

--- nodeinpractice/node-techniques/ch2/3/app.js
+++ nodeinpractice/node-techniques/ch2/3/app.js
@@ -1,8 +1,9 @@
 /**
+ * Technique 3: Loading a group of related modules
+ *
  * Problem:
  * you want to group related files together under a directory and only have to
  * load it with one call to require
- *
  * Solution:
  * create a file called index.js to load each module and export them as a group
  * or add a package.json file to the directory
---
+++ nodeinpractice/node-techniques/ch2/4/paths.js
@@ -0,0 +1,18 @@
+/**
+ * Technique 4: Working with paths
+ *
+ * Problem:
+ * You want to access a file that isn't handled by the module system
+ * Solution:
+ * Use __dirname or __filename
+ *
+ * most developers join these variables with a path frament using simple
+ * string concatenation: var view = __dirname + '/views/view.html';
+ *
+ * to make sure paths are joined correctly, you can use node's path
+ * module: path.joined
+ * ex: (__dirname, 'views', 'view.html');
+ */
+
+console.log('__dirname:', __dirname);
+console.log('__filename:', __filename);
