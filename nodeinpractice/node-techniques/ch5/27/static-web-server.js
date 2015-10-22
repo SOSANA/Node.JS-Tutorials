4e78003ae1a8d71d6779938cbaf7340ecd693c0b
sosana <thesosana@gmail.com>
2015-10-22 15:35:09 -0600

    updating stream modules

---
+++ nodeinpractice/node-techniques/ch5/27/static-web-server.js
@@ -0,0 +1,17 @@
+/**
+ * Technique X:
+ * 	-
+ *
+ * Problem:
+ * 	-
+ * Solution:
+ * 	-
+ *
+ * 	-
+ */
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+  fs.createReadStream(__dirname + '/index.html').pipe(res); //<co id="callout-streams-static-web-server" />
+}).listen(8000);
---
+++ nodeinpractice/node-techniques/ch5/30/readable.js
@@ -0,0 +1,20 @@
+/**
+ * Technique X:
+ * 	-
+ *
+ * Problem:
+ * 	-
+ * Solution:
+ * 	-
+ *
+ * 	-
+ */
+var Readable = require('stream').Readable;
+
+function MyStream(options) {
+  Readable.call(this, options); //<co id="callout-streams-inheritance-1" />
+}
+
+MyStream.prototype = Object.create(Readable.prototype, { //<co id="callout-streams-inheritance-2" />
+  constructor: { value: MyStream }
+});
