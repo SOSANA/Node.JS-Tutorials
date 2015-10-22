4c36ad93a719516ed09ed2ef57a6b3749227549f
sosana <thesosana@gmail.com>
2015-10-22 13:29:38 -0600

    mixing in EventEmitter, using for-loop to copy the EventEmitter properties

--- nodeinpractice/node-techniques/ch4/20/mixin-1.js
+++ nodeinpractice/node-techniques/ch4/20/mixin-1.js
@@ -1,21 +1,26 @@
 /**
- * Technique X:
- * 	-
+ * Technique 20: Mixing in EventEmitter
+ * 	- sometimes inheritance isn't the right way to use EventEmitter. In these
+ * 		cases, mixing in EventEmitter may work
  *
  * Problem:
- * 	-
+ * 	- This is an alternative option to technique 19. Rather than using EventEmitter
+ * 		as a base class, it's possible to copy its methods into another class, This
+ * 		is useful when you have an existing class and can't easily rework it to
+ * 		inherit directly from EventEmitter
  * Solution:
- * 	-
+ * 	- Using a for-in loop is sufficient for coping the properties from one prototype
+ * 		to another. In this way you can copy the necessary properties from EventEmitter
  *
- * 	-
+ * 	- this approach is more akin to a mixin, or multiple inheritance
  */
 var EventEmitter = require('events').EventEmitter;
 
 function MusicPlayer(track) {
   this.track = track;
   this.playing = false;
-
-  for (var methodName in EventEmitter.prototype) { //<co id="callout-events-mixin-1" />
+  // this is the for-in loop that copies the relevant properties
+  for (var methodName in EventEmitter.prototype) {
     this[methodName] = EventEmitter.prototype[methodName];
   }
 }
