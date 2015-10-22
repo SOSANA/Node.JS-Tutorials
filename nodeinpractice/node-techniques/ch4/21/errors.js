87a6f3b3c24b3a7ffe0390a2f0c4ae29bfe6570a
sosana <thesosana@gmail.com>
2015-10-22 13:35:34 -0600

    managing errors by binding a listener to the error event

--- nodeinpractice/node-techniques/ch4/21/errors.js
+++ nodeinpractice/node-techniques/ch4/21/errors.js
@@ -1,13 +1,17 @@
 /**
- * Technique X:
- * 	-
+ * Technique 21: Managing errors
+ * 	- error handling with EventEmitter has its own special rules that must be
+ * 		adhered to. This technique explains how error handling works
  *
  * Problem:
- * 	-
+ * 	- your're using EventEmitterand want to gracefully handle when errors occur,
+ * 		but it keeps raising exceptions
  * Solution:
- * 	-
+ * 	- to prevent EventEmitter from throwing exceptions whenever an error event
+ * 		is emitted, add a listner to the error event. This can be done with
+ * 		custom classes or any standard class that inherits from EventEmitter
  *
- * 	-
+ * 	- to handle errors, bind a listner to the error event
  */
 var util = require('util');
 var events = require('events');
@@ -23,8 +27,8 @@ var musicPlayer = new MusicPlayer();
 musicPlayer.on('play', function(track) {
   this.emit('error', 'unable to play!');
 });
-
-musicPlayer.on('error', function(err) { //<co id="callout-events-errors-1-1" />
+// listening for an error event
+musicPlayer.on('error', function(err) { 
   console.error('Error:', err);
 });
 
