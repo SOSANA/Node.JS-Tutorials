00cdf61688d12c600007144de943a774b139db57
sosana <thesosana@gmail.com>
2015-10-22 14:26:51 -0600

    using alternatives to EventEmitter

--- nodeinpractice/node-techniques/ch4/26/rabbitmq.js
+++ nodeinpractice/node-techniques/ch4/26/rabbitmq.js
@@ -1,13 +1,24 @@
 /**
- * Technique X:
- * 	-
+ * Technique 26: Alternatives to EventEmitter
+ * 	- EventEmitter has a great API and works well in Node programs, but
+ * 		sometimes a problem requires a slightly different solution.
  *
  * Problem:
- * 	-
+ * 	- your trying to solve a problem that doesn't quite fit EventEmitter
  * Solution:
- * 	-
+ * 	- depending on the exact nature of the problem your trhying to solve, there
+ * 		are several alternatives to EventEmitter such as publish/subscribe, AMQP,
+ * 		and js-signals are some popular alternatives with good support in Node
  *
- * 	-
+ * 	- the EventEmitter classs is an implementation of the observer pattern, A
+ * 		related pattern is publish/subscribe, where publishers send messages that
+ * 		are characterized into classes to subscribers without knowsing the details
+ * 		of the subscribers themselves
+ * 	- the publish/subscribe pattern is often useful in cases wher ehorizontal
+ * 		scaling is required. If you need to run multiple Node processes on multiple
+ * 		servers, then technologies like AMQP and 0MQ can help implement this. They
+ * 		are both specifically designed to solve this class of problem, but may not
+ * 		be as conveient as using the Redis publish/subscribe API
  */
 var rabbitHub = require('rabbitmq-nodejs-client');
 var subHub = rabbitHub.create( { task: 'sub', channel: 'myChannel' } );
