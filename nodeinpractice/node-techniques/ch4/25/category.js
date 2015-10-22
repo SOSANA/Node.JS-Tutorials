5a0de0d171b04afcdd6631dfebad2f72d5b99eab
sosana <thesosana@gmail.com>
2015-10-22 14:16:38 -0600

    categorizing event names using an object to efficiently manage events

--- nodeinpractice/node-techniques/ch4/25/category.js
+++ nodeinpractice/node-techniques/ch4/25/category.js
@@ -1,13 +1,19 @@
 /**
- * Technique X:
- * 	-
+ * Technique 25: Categorizing event names
+ * 	- some projects just have too many events. This technique shows how to
+ * 		deals with bugs caused by mistyped event names
  *
  * Problem:
- * 	-
+ * 	- your losing track of the events in your program, and are concerned that
+ * 		it may be too easy to write an incorrect event name somewhere causing a
+ * 		difficult-to-track bug
  * Solution:
- * 	-
+ * 	- the easiest way to solve this problem is to use an object to act as central
+ * 		dictionary for all the event names. This creates a centralized location of
+ * 		each event in the project
  *
- * 	-
+ * 	- its hard to keep track of event names littered throughout a project. one
+ * 		way to manage this is to keep each event name in one place
  */
 var util = require('util');
 var events = require('events');
@@ -16,14 +22,14 @@ function MusicPlayer() {
   events.EventEmitter.call(this);
   this.on(MusicPlayer.events.play, this.play.bind(this));
 }
-
-var e = MusicPlayer.events = { //<co id="callout-events-category-1" />
-  play: 'play'
-, pause: 'pause'
-, stop: 'stop'
-, ff: 'ff'
-, rw: 'rw'
-, addTrack: 'add-track'
+// the object used to store the event list is aliased for convenience
+var e = MusicPlayer.events = {
+  play: 'play',
+  pause: 'pause',
+  stop: 'stop',
+  ff: 'ff',
+  rw: 'rw',
+  addTrack: 'add-track'
 };
 
 util.inherits(MusicPlayer, events.EventEmitter);
@@ -33,8 +39,9 @@ MusicPlayer.prototype.play = function() {
 };
 
 var musicPlayer = new MusicPlayer();
-
-musicPlayer.on(e.play, function() {  //<co id="callout-events-category-2" />
+// when adding new listeners, users of the class can refer to the events list
+// rather than writing the event names as strings
+musicPlayer.on(e.play, function() {
   console.log('Now playing');
 });
 
