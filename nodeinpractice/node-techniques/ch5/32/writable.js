34501aacd83dcd957eb82a7597f9ae59bfb1ca08
sosana <thesosana@gmail.com>
2015-10-23 11:46:10 -0600

    Implementing a writable stream

--- nodeinpractice/node-techniques/ch5/32/writable.js
+++ nodeinpractice/node-techniques/ch5/32/writable.js
@@ -1,16 +1,18 @@
 /**
- * Technique X:
- * 	-
+ * Technique 32: Implementing a writable stream
+ * 	- writable streams can be used to output data to underlying I/O sinks
  *
  * Problem:
- * 	-
+ * 	- you want to output data from a program using an I/O destination that you
+ * 		want to wrap with a streamable interface
  * Solution:
- * 	-
+ * 	- inherit from stream.Writable and implement a _write method to send data
+ * 		to the underlying resource
  *
- * 	-
+ * 	- using pipe API helps keep chunks of code reusable and decoupled
  */
 var stream = require('stream');
-//<co id="callout-streams-writable-1" />
+// use the usual inheritance pattern to create a new writable stream class
 GreenStream.prototype = Object.create(stream.Writable.prototype, {
   constructor: { value: GreenStream }
 });
@@ -20,8 +22,10 @@ function GreenStream(options) {
 }
 
 GreenStream.prototype._write = function(chunk, encoding, callback) {
-  process.stdout.write('\u001b[32m' + chunk + '\u001b[39m'); //<co id="callout-streams-writable-2" />
-  callback(); //<co id="callout-streams-writable-3" />
+  // decorate the chunk with the ANSI escape sequences for green text
+  process.stdout.write('\u001b[32m' + chunk + '\u001b[39m');
+  // call the callback once the text has been sent to stdout
+  callback();
 };
-
-process.stdin.pipe(new GreenStream()); //<co id="callout-streams-writable-4" />
+// pipe stdin through stdout to transform text into green text
+process.stdin.pipe(new GreenStream());
