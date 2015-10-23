bbf1e7b8ae5194f1290e45bdffef1e91da92a9e0
sosana <thesosana@gmail.com>
2015-10-23 11:15:30 -0600

    Correctly inheriting from the stream base classes

--- nodeinpractice/node-techniques/ch5/30/readable.js
+++ nodeinpractice/node-techniques/ch5/30/readable.js
@@ -1,20 +1,25 @@
 /**
- * Technique X:
- * 	-
+ * Technique 30: Correctly inheriting from the stream base classes
+ * 	- node's base classes for streams can be used as a starting point for new
+ * 		modules and subclasses. It's important to understand what problems each
+ * 		solves, and how to correctly inherit from them
  *
  * Problem:
- * 	-
+ * 	- you want to solve a problem by creting a streamable API, but your're
+ * 		not sure which base class to use and how to use it
  * Solution:
- * 	-
+ * 	- decide on which base classs closely matches the problem at hand and
+ * 		inherit from it using Object.prototype.call and util.inherits();
  *
  * 	-
  */
 var Readable = require('stream').Readable;
 
 function MyStream(options) {
-  Readable.call(this, options); //<co id="callout-streams-inheritance-1" />
+  // call the parent constructor, and be sure to pass any options to it as well
+  Readable.call(this, options);
 }
-
-MyStream.prototype = Object.create(Readable.prototype, { //<co id="callout-streams-inheritance-2" />
+// use Object.create to correctly set up the prototype chain
+MyStream.prototype = Object.create(Readable.prototype, {
   constructor: { value: MyStream }
 });
