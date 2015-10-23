c9997ecf4dc7088c928dde5636ba91aa2246da2a
sosana <thesosana@gmail.com>
2015-10-23 10:51:52 -0600

    Using streams from third-party modules

--- nodeinpractice/node-techniques/ch5/29/express.js
+++ nodeinpractice/node-techniques/ch5/29/express.js
@@ -1,20 +1,24 @@
 /**
- * Technique X:
- * 	-
+ * Technique 29: Using streams from third-party modules
+ * 	- many open source developers have recognized the importance of streams
+ * 		and incorporated streamable interfaces into their modules. In this
+ * 		technique you'll learn how to identify such implementations and use them
+ * 		to solve problems more efficiently
  *
  * Problem:
- * 	-
+ * 	- you want to know how to use streams with a popular third-party module
+ * 		that you've downloaded with npm
  * Solution:
- * 	-
- *
- * 	-
+ * 	- look at the module's documentation or src code to figure out if it
+ * 		implements a streamable API, and if so, how to use it
  */
 var stream = require('stream');
 var util = require('util');
 var express = require('express');
 var app = express();
-
-util.inherits(StatStream, stream.Readable); //<co id="callout-streams-express-1" />
+// create a readable stream by inheriting from stream.Readable and calling
+// the parent's constructor
+util.inherits(StatStream, stream.Readable);
 
 function StatStream(limit) {
   stream.Readable.call(this);
@@ -26,7 +30,10 @@ StatStream.prototype._read = function(size) {
     // Done
     this.push();
   } else {
-    this.push(util.inspect(process.memoryUsage())); //<co id="callout-streams-express-2" />
+    // respond with some data, this sends a string representation of the node
+    // process's memory usage
+    this.push(util.inspect(process.memoryUsage()));
+    // creating a newline character
     this.push('\n');
     this.limit--;
   }
@@ -34,7 +41,9 @@ StatStream.prototype._read = function(size) {
 
 app.get('/', function(req, res) {
   var statStream = new StatStream(10);
-  statStream.pipe(res); //<co id="callout-streams-express-3" />
+  // use the standard readable.pipe(writable) pattern to send data back to the
+  // browser
+  statStream.pipe(res);
 });
 
 app.listen(3000);
