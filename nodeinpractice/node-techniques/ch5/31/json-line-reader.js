1f56249a5f319c4d1ed141598e2f6668a4533a78
sosana <thesosana@gmail.com>
2015-10-23 11:30:38 -0600

    Implementing a readable stream

--- nodeinpractice/node-techniques/ch5/31/json-line-reader.js
+++ nodeinpractice/node-techniques/ch5/31/json-line-reader.js
@@ -1,38 +1,46 @@
 /**
- * Technique X:
- * 	-
+ * Technique 31: Implementing a readable stream
+ * 	- readable streams can be used to provide a flexiable API around I/O
+ * 		sources, and also act as parsers
  *
  * Problem:
- * 	-
+ * 	- you'd like to wrap an I/O source with a streamable API that provides
+ * 		a higher-level interface than would otherwise be posssible with the
+ * 		underlying data
  * Solution:
- * 	-
+ * 	- implement a readable stream by inheriting from the stream.Readable
+ * 		class and creating a _read(size) method
  *
- * 	-
+ * 	- implementing a custom stream.Readable class can be useful when a
+ * 		higher level of abstraction around an underlying data source is
+ * 		required
  */
 var stream = require('stream');
 var util = require('util');
 var fs = require('fs');
 
 function JSONLineReader(source) {
-  stream.Readable.call(this); //<co id="callout-streams-json-line-1" />
+  // always ensure the constructors parent is called
+  stream.Readable.call(this);
   this._source = source;
   this._foundLineEnd = false;
   this._buffer = '';
 
   source.on('readable', function() {
-    this.read(); //<co id="callout-streams-json-line-2" />
+    // call read() when the source is ready to trigger a subsequent reads
+    this.read();
   }.bind(this));
 }
-
-util.inherits(JSONLineReader, stream.Readable); //<co id="callout-streams-json-line-3" />
-
-JSONLineReader.prototype._read = function(size) { //<co id="callout-streams-json-line-4" />
+// inherit from stream.Readable to create a new class that can be customized
+util.inherits(JSONLineReader, stream.Readable);
+// all custom stream.Readable classes must implement the _read() method
+JSONLineReader.prototype._read = function(size) {
   var chunk;
   var line;
   var lineIndex;
   var result;
-
-  if (this._buffer.length === 0) { //<co id="callout-streams-json-line-5" />
+  // when the class is ready for more data, call read() on the source
+  if (this._buffer.length === 0) {
     chunk = this._source.read();
     this._buffer += chunk;
   }
@@ -40,12 +48,17 @@ JSONLineReader.prototype._read = function(size) { //<co id="callout-streams-json
   lineIndex = this._buffer.indexOf('\n');
 
   if (lineIndex !== -1) {
-    line = this._buffer.slice(0, lineIndex); //<co id="callout-streams-json-line-6" />
+    // slice from the start of the bufffer to the first newline to grab
+    // some text to parse
+    line = this._buffer.slice(0, lineIndex);
     if (line) {
       result = JSON.parse(line);
       this._buffer = this._buffer.slice(lineIndex + 1);
-      this.emit('object', result); //<co id="callout-streams-json-line-7" />
-      this.push(util.inspect(result)); //<co id="callout-streams-json-line-8" />
+      // emitting an 'object' event whenever a JSON record has been parsed
+      // is unique to this class and isn't necessarily required
+      this.emit('object', result);
+      // send th parsed JSON back to the internal queue
+      this.push(util.inspect(result));
     } else {
       this._buffer = this._buffer.slice(1);
     }
@@ -54,7 +67,8 @@ JSONLineReader.prototype._read = function(size) { //<co id="callout-streams-json
 
 var input = fs.createReadStream(__dirname + '/json-lines.txt', {
   encoding: 'utf8'
-}); //<co id="callout-streams-json-line-9" />
+});
+// create an instance of jsonLineReader and give it a file stream to process
 var jsonLineReader = new JSONLineReader(input);
 
 jsonLineReader.on('object', function(obj) {
