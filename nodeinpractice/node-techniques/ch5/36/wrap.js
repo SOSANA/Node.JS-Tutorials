fae42a6600fdcb3760902ad7be3b930cad663753
sosana <thesosana@gmail.com>
2015-10-23 13:24:27 -0600

    using the old streams API

--- nodeinpractice/node-techniques/ch5/36/wrap.js
+++ nodeinpractice/node-techniques/ch5/36/wrap.js
@@ -1,13 +1,13 @@
 /**
- * Technique X:
- * 	-
+ * Technique 36: Using the old streams API
+ * 	- before node 0.10 streams had a different API. Code written using API can
+ * 		be used with the newer APIs by wrapping it to behave like the newer
+ * 		stream.Readable class
  *
  * Problem:
- * 	-
+ * 	- you want to use a module that implements the old-style stream API
  * Solution:
- * 	-
- *
- * 	-
+ * 	- use Readable.prototype.wrap
  */
 var stream = require('stream');
 var Readable = stream.Readable;
@@ -16,16 +16,16 @@ var util = require('util');
 util.inherits(MemoryStream, stream);
 
 function MemoryStream(interval) {
-  this.readable = true; //<co id="callout-streams-wrap-1" />
+  this.readable = true;
 
   setInterval(function() {
     var data = process.memoryUsage();
     data.date = new Date();
-    this.emit('data', JSON.stringify(data) + '\n'); //<co id="callout-streams-wrap-2" />
+    this.emit('data', JSON.stringify(data) + '\n');
   }.bind(this), interval);
 }
 
 var memoryStream = new MemoryStream(250);
-var wrappedStream = new Readable().wrap(memoryStream); //<co id="callout-streams-wrap-3" />
+var wrappedStream = new Readable().wrap(memoryStream);
 
-wrappedStream.pipe(process.stdout); //<co id="callout-streams-wrap-4" />
+wrappedStream.pipe(process.stdout);
