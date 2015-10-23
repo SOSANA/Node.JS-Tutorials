30e3c5842f967f49419400e9cd9caa24b4e93e7b
sosana <thesosana@gmail.com>
2015-10-23 10:29:11 -0600

    using built-in streams to make a static server

--- nodeinpractice/node-techniques/ch5/27/static-web-gz.js
+++ nodeinpractice/node-techniques/ch5/27/static-web-gz.js
@@ -1,10 +1,20 @@
+/**
+ * 	- using gzip to compress files
+ * 	- we could of piped through an HTML templating engine and then compressed.
+ * 	- Just remember that general pattern is readable.pipe(writable)
+ */
+
 var http = require('http');
 var fs = require('fs');
 var zlib = require('zlib');
 
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'content-encoding': 'gzip' }); //<co id="callout-streams-static-web-gz-1" />
+  // set the header so the browser knows gzip and compression has been used
+  res.writeHead(200, { 'content-encoding': 'gzip' });
   fs.createReadStream(__dirname + '/index.html')
-    .pipe(zlib.createGzip())//<co id="callout-streams-static-web-gz-2" />
+    // use two calls to pipe: compress, and stream the file back to the client
+    .pipe(zlib.createGzip())
     .pipe(res);
 }).listen(8000);
+
+console.log('server listening on localhost:8000/');
--- nodeinpractice/node-techniques/ch5/27/static-web-server.js
+++ nodeinpractice/node-techniques/ch5/27/static-web-server.js
@@ -1,17 +1,25 @@
 /**
- * Technique X:
- * 	- 
+ * Technique 27: Using built-in streams to make a static web server
+ * 	- node's core modules often have streamabnle interfaces. They can be used to
+ * 		solve many problems more efficiently than their synchronous alternatives
  *
  * Problem:
- * 	-
+ * 	- you want to send a file from a web server to a client in a efficient manner
+ * 		that will cale up to large files
  * Solution:
- * 	-
+ * 	- use 'fs.createReadStream' to open a file and stream it to the client
+ * 	- optionally, pipe the resulting stream.Readable through another stream to
+ * 		handle features like compression
  *
- * 	-
+ * 	- node's core modules for file system and network operations, fs and net,
+ * 		both provide streamable interfaces. The fs module has helper methods to
+ * 		automatically create instances of the streamable classes. This makes using
+ * 		streams for some I/O based problems fairly straight forward.
  */
 var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res) {
-  fs.createReadStream(__dirname + '/index.html').pipe(res); //<co id="callout-streams-static-web-server" />
+  // data is piped from a file to node's HTTP response object
+  fs.createReadStream(__dirname + '/index.html').pipe(res);
 }).listen(8000);
