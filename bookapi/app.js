6eb9c66442e8fb5d3e2448b92bb39c1a4eb87d53
sosana <thesosana@gmail.com>
2015-11-02 13:47:25 -0700

    updating to use express router

--- bookapi/app.js
+++ bookapi/app.js
@@ -4,6 +4,16 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+  .get(function (req,res) {
+    var responseJson = {hello: "This is my api"};
+    res.json(responseJson);
+  });
+
+app.use('/api', bookRouter);
+
 app.get('/', function(req, res) {
   res.send('Welcome to my API!');
 });
