4e181e05d28e4d1d0b32ae1a5da390dac1b9d533
sosana <thesosana@gmail.com>
2015-11-17 16:55:45 -0700

    updated schema to include validators and error msgs

--- pls_mongoose/models/standup.server.model.js
+++ pls_mongoose/models/standup.server.model.js
@@ -1,12 +1,52 @@
 var mongoose = require( 'mongoose' );
 var Schema = mongoose.Schema;
 
+var memberNameValidator = [
+  function (val) {
+    // returning weither or no our value passed in is longer than 0 characters
+    // and does not equal 'none'
+    return (val.length > 0 && val.toLocaleLowerCase() != 'none');
+  },
+  // custom error text
+  'Select a valid member name.'
+];
+
+var requiredStringValidator = [
+  // trims out any spaces in text
+  function (val) {
+    var testval = val.trim();
+    return (testval.length > 0);
+  },
+  // custom error text
+  '{PATH} cannot be empty'
+];
+
 var standupSchema = new Schema({
-    memberName: String,
-    project: String,
-    workYesterday: String,
-    workToday: String,
-    impediment: String,
+    memberName: {
+      type: String,
+      required: true,
+      validate: memberNameValidator
+    },
+    project: {
+      type: String,
+      required: true,
+      validate: requiredStringValidator
+    },
+    workYesterday: {
+      type: String,
+      required: true,
+      validate: requiredStringValidator
+    },
+    workToday: {
+      type: String,
+      required: true,
+      validate: requiredStringValidator
+    },
+    impediment: {
+      type: String,
+      required: true,
+      default: 'none'
+    },
     createdOn: { type: Date, default: Date.now }
 });
 
