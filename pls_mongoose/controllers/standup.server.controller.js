419d09c180986f56a19b22d0c849f48cda3bbb40
sosana <thesosana@gmail.com>
2015-11-17 15:31:24 -0700

    created new methods for filter and list

--- pls_mongoose/controllers/standup.server.controller.js
+++ pls_mongoose/controllers/standup.server.controller.js
@@ -1,7 +1,35 @@
 var Standup = require('../models/standup.server.model.js');
 
-// method 'create'
-exports.create = function(req, res) {
+// creating a method called 'list'
+exports.list = function (req, res) {
+  var query = Standup.find();
+  // in our index.html we have a swig for loop that gets its data
+  // from notes: result
+  query.sort({ createdOn: 'desc'})
+       .limit(12)
+       .exec(function (err, results) {
+         res.render('index', { title: 'Standup - List', notes: results});
+       });
+};
+
+// creating a method called 'filterByMember'
+exports.filterByMember = function (req, res) {
+  var query = Standup.find();
+  var filter = req.body.memberName;
+
+  query.sort({ createdOn: 'desc'});
+
+  if (filter.length > 0) {
+    query.where({ memberName: filter});
+  }
+
+  query.exec(function (err, results) {
+    res.render('index', { title: 'Standup - List', notes: results });
+  });
+};
+
+// creating a method called 'create'
+exports.create = function (req, res) {
   var entry = new Standup({
     memberName: req.body.memberName,
     project: req.body.project,
@@ -17,7 +45,7 @@ exports.create = function(req, res) {
   res.redirect(301, '/');
 };
 
-// method 'getNote'
-exports.getNote = function(req, res) {
+// creating a method called 'getNote'
+exports.getNote = function (req, res) {
   res.render('newnote', { title: 'Standup - New Note'});
 };
