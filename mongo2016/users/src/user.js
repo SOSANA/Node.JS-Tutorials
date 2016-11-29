9661082a2866dabfd436a3347a8bb20fe134d0a5
sosana <thesosana@gmail.com>
2016-11-29 09:03:34 -0800

    creating user model

--- mongo2016/users/src/user.js
+++ mongo2016/users/src/user.js
@@ -1,3 +1,11 @@
-/**
- * update me
- */
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: String
+})
+
+const User = mongoose.model('user', UserSchema);
+
+export default User;
