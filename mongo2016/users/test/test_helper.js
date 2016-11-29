0621c4c1192065cf51822631fbc409fb47a78f7b
sosana <thesosana@gmail.com>
2016-11-29 08:41:34 -0800

    updating our mongodb url

--- mongo2016/users/test/test_helper.js
+++ mongo2016/users/test/test_helper.js
@@ -1,6 +1,8 @@
+// const mongoose = require('mongoose');
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost/users_test');
+
+mongoose.connect('mongodb://localhost/mongo2016_users_test');
 mongoose.connection
   .once('open', () => { console.log('Good to go!'); })
   .on('error', (error) => {
