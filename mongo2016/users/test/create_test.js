7bb5d5e4320ca5b800dcc516fa6e3a5bb7b34faf
sosana <thesosana@gmail.com>
2016-12-06 14:19:06 -0800

    updating our create test mock

--- mongo2016/users/test/create_test.js
+++ mongo2016/users/test/create_test.js
@@ -6,8 +6,16 @@ import assert from 'assert';
 import User from '../src/user';
 
 describe('Creating records', () => {
-  it('saves a user', () => {
-    // compares one value to another
+  it('saves a user', (done) => {
     const joe = new User({ name: 'Joe' }); // creating new instance of User
+    joe.save()
+      .then(() => { // has successfully saved
+        // assert compares one value to another, mongoose provides isNew function
+        assert(!joe.isNew);
+        done(); // make sure to call it in our spec/it block
+      })
+      .catch(() => {
+
+      });
   });
 });
