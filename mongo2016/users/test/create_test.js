1e355b543c9750a55c325613dd7146eacbd79701
sosana <thesosana@gmail.com>
2016-12-05 18:30:23 -0800

    adding create test

--- mongo2016/users/test/create_test.js
+++ mongo2016/users/test/create_test.js
@@ -1,3 +1,13 @@
 /**
- * update me
+ * purpose of this test is to make sure it correctly saves to the db
  */
+
+import assert from 'assert';
+import User from '../src/user';
+
+describe('Creating records', () => {
+  it('saves a user', () => {
+    // compares one value to another
+    const joe = new User({ name: 'Joe' }); // creating new instance of User
+  });
+});
