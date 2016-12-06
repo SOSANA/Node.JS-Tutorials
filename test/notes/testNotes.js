1efb33294c08f8fb9904599c3f90a12ab29d04a9
sosana <thesosana@gmail.com>
2016-12-05 18:29:36 -0800

    updating dependencies

--- test/notes/testNotes.js
+++ test/notes/testNotes.js
@@ -1,3 +1,19 @@
 /**
- * add notes later :)
+ * Sinon:
+ *  - Sinon helps eliminate complexity in tests by allowing you to easily create
+ *    so called test-double.
+ *    Test-doubles:
+ *     - are, like the name suggests, replacements for pieces of code used in your
+ *       tests. Taking this idea a little bit further. With Sinon, we can replace
+ *       any JavaScript function with a test-double, which can then be configured
+ *       to do a variety of things to make testing complex things simple.
+ *  - Sinon splits test-doubles into three types:
+ *    Spies:
+ *     - which offer information about function calls, without affecting their behavior
+ *    Stubs:
+ *     - which are like spies, but completely replace the function. This makes it
+ *       possible to make a stubbed function do whatever you like — throw an exception,
+ *       return a specific value, etc
+ *    Mocks:
+ *     - which make replacing whole objects easier by combining both spies and stubs
  */
