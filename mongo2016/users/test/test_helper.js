a4a0d0dde07e7e616cc6b442a9befd046bb0bf61
sosana <thesosana@gmail.com>
2016-12-06 14:18:21 -0800

    updating test helper with before/Each hooks

--- mongo2016/users/test/test_helper.js
+++ mongo2016/users/test/test_helper.js
@@ -1,10 +1,28 @@
 // const mongoose = require('mongoose');
 import mongoose from 'mongoose';
 
+// setting es6 promises by default for mongoose
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/mongo2016_users_test');
-mongoose.connection
-  .once('open', () => { console.log('Good to go!'); })
-  .on('error', (error) => {
-    console.warn('Error', error);
+// runs and connects to mongo one time
+before((done) => {
+  mongoose.connect('mongodb://localhost/mongo2016_users_test');
+  mongoose.connection
+    .once('open', () => {
+      // console.log('Good to go!');
+      done();
+    })
+    .on('error', (error) => {
+      console.warn('Error', error);
+    });
+});
+
+// a hook is a function that will be executed before any test gets executed
+// inside the test suite. You need to pass done callback
+beforeEach((done) => {
+  // take all the records inside the users collections and get rid of them
+  mongoose.connection.collections.users.drop(() => {
+    // ready to run the next test! sends a signal to mocha that its done
+    done();
   });
+});
