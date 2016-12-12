65aa2d42df51738a632d791f7f8b7b369404fc53
sosana <thesosana@gmail.com>
2016-12-12 15:37:12 -0800

    updating spec to mock saving and removing subdoc's

--- mongo2016/users/test/subdocument_test.js
+++ mongo2016/users/test/subdocument_test.js
@@ -19,4 +19,43 @@
         done();
       });
    });
+
+   it('Can add subdocuments to an existing record', (done) => {
+     const joe = new User({ // create joe model
+       name: 'Joe',
+       posts: []
+     });
+
+     joe.save() // save joe
+      .then(() => User.findOne({ name: 'Joe' })) // fetch joe
+      .then((user) => { // add new record
+        user.posts.push({ title: 'New Post' });
+        return user.save(); // saving joe
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'New Post');
+        done();
+      });
+   });
+
+   it('can remove an existing subdocument', (done) => {
+     const joe = new User({
+       name: 'Joe',
+       posts: [{ title: 'New Post' }]
+     });
+
+     joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        const post = user.posts[0];
+        post.remove();
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      });
+   });
  });
