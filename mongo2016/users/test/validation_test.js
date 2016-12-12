a257eb788f8e652727681bb6d5966e05a3ab4896
sosana <thesosana@gmail.com>
2016-12-12 13:34:29 -0800

    updating spec for disallowing invalid records

--- mongo2016/users/test/validation_test.js
+++ mongo2016/users/test/validation_test.js
@@ -9,7 +9,6 @@
    it('requires a user name', () => {
      const user = new User({ name: undefined });
      const validationResult = user.validateSync(); // not doing async validation here
-     // use this to get error msg
      // const message = validationResult.errors.name.message; // es5
      const { message } = validationResult.errors.name;
      assert(message === 'Name is required.');
@@ -21,5 +20,15 @@
      const { message } = validationResult.errors.name;
 
      assert(message === 'Name must be longer than 2 characters.');
+   });
+
+   it('disallows invalid records from being saved', (done) => {
+     const user = new User({ name: 'Al' });
+     user.save()
+      .catch((validationResult) => {
+        const { message } = validationResult.errors.name;
+        assert(message === 'Name must be longer than 2 characters.');
+        done();
+      });
    });
  });
