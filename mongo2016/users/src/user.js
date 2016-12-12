9bb40f01b6d040517f8def6e59610f5f91be1ab0
sosana <thesosana@gmail.com>
2016-12-12 13:24:10 -0800

    updating schema for validation

--- mongo2016/users/src/user.js
+++ mongo2016/users/src/user.js
@@ -3,7 +3,14 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    validate: {
+      validator: name => name.length > 2,
+      message: 'Name must be longer than 2 characters.'
+    },
+    required: [true, 'Name is required.']
+  },
   postCount: Number
 });
 
