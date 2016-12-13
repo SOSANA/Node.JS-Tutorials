469b2eeef697cc4968e6b7f0efc44b9e50195f6c
sosana <thesosana@gmail.com>
2016-12-12 17:25:43 -0800

    adding blog post model

---
+++ mongo2016/users/src/blogPost.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const BlogPostSchema = new Schema({
+  title: String,
+  content: String,
+  comments: [{ type: Schema.Types.ObjectId, ref: 'comment' }]
+});
+
+const BlogPost = mongoose.model('blogPost', BlogPostSchema);
+
+export default BlogPost;
