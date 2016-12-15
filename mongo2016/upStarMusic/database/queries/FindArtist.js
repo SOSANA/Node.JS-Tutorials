48d8486aec78fee14cc52213ab06727ce4803b4a
sosana <thesosana@gmail.com>
2016-12-15 15:12:40 -0800

    find single artist query

--- mongo2016/upStarMusic/database/queries/FindArtist.js
+++ mongo2016/upStarMusic/database/queries/FindArtist.js
@@ -1,9 +1,12 @@
-const Artist = require('../models/artist');
+import Artist from '../models/artist';
 
 /**
  * Finds a single artist in the artist collection.
  * @param {string} _id - The ID of the record to find.
  * @return {promise} A promise that resolves with the Artist that matches the id
  */
-module.exports = (_id) => {
+
+export default (_id) => {
+  // return Artist.findOne({ _id }); // could use this as alternative
+  return Artist.findById({ _id });
 };
