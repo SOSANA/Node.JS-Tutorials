8550c593a1b6571e1a35270d94e3f2ba28c09091
sosana <thesosana@gmail.com>
2016-12-15 14:47:05 -0800

    creating Album Schema

--- mongo2016/upStarMusic/database/models/album.js
+++ mongo2016/upStarMusic/database/models/album.js
@@ -1,1 +1,15 @@
 // Todo: create Album Schema
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: { type: String },
+  date: { type: Date, default: Date.now },
+  copiesSold: { type: Number },
+  numberTracks: { type: Number },
+  image: { type: String },
+  revenue: { type: Number }
+});
+
+export default AlbumSchema;
