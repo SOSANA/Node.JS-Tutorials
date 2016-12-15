4e830cab02f568dbc1d5f5bddf07e6f5db10abba
sosana <thesosana@gmail.com>
2016-12-15 14:56:24 -0800

    creating Artist Model

--- mongo2016/upStarMusic/database/models/artist.js
+++ mongo2016/upStarMusic/database/models/artist.js
@@ -1,1 +1,22 @@
 // Todo: Create Artist Model
+import mongoose from 'mongoose';
+import AlbumSchema from './album';
+
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+  name: { type: String },
+  age: { type: Number },
+  yearsActive: { type: Number },
+  image: { type: String },
+  genre: { type: String },
+  website: { type: String },
+  netWorth: { type: Number },
+  labelName: { type: String },
+  retired: { type: Boolean },
+  albums: [AlbumSchema]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+
+export default Artist;
