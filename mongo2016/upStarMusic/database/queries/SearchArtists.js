1c703159242c5b16e9535fc0d45f7912b144fc26
sosana <thesosana@gmail.com>
2017-01-10 14:29:23 -0800

    updating search query

--- mongo2016/upStarMusic/database/queries/SearchArtists.js
+++ mongo2016/upStarMusic/database/queries/SearchArtists.js
@@ -10,5 +10,20 @@ import Artist from '../models/artist';
  */
 
 export default (criteria, sortProperty, offset = 0, limit = 20) => {
+  // es5
+  // const sortOrder = {};
+  // sortOrder[sortProperty] = 1;
+  // Artist.find({})
+  //   .sort(sortOrder);
 
+  // es6
+  const query = Artist.find({})
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.count()])
+    .then((results) => {
+      return { all: results[0], count: results[1], offset, limit };
+    });
 };
