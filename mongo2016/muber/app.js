254494bdb3049c47d92a107da282faf8fa53f438
sosana <thesosana@gmail.com>
2017-01-17 14:44:18 -0800

    adding mongoose

--- mongo2016/muber/app.js
+++ mongo2016/muber/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/muber');
 
 app.use(bodyParser.json());
 
@@ -11,6 +15,5 @@ routes(app);
 app.listen(3050, () => { // eslint-disable-line
   console.log('Running on port: 3050'); // eslint-disable-line
 });
-
 
 export default app;
