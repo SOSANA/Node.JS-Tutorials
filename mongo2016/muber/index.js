fc58eb256ef7ce38d3773a43f4b8b324dfadf42d
sosana <thesosana@gmail.com>
2017-01-16 16:11:03 -0800

    updating app.js require

--- mongo2016/muber/index.js
+++ mongo2016/muber/index.js
@@ -1,6 +1,6 @@
 require('babel-register');
 require('babel-polyfill');
-import app from './app';
+require('./app');
 
 app.listen(3050, () => { // eslint-disable-line
   console.log('Running on port: 3050'); // eslint-disable-line
