63cd1f67772447f0be4c8fa928196af5003bb74e
sosana <thesosana@gmail.com>
2017-01-18 15:17:19 -0800

    applying NODE_ENV variable for development

--- mongo2016/muber/app.js
+++ mongo2016/muber/app.js
@@ -6,7 +6,10 @@ import routes from './routes';
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/muber');
+
+if (process.env.NODE_ENV === 'development') {
+  mongoose.connect('mongodb://localhost/muber');
+}
 
 app.use(bodyParser.json());
 
