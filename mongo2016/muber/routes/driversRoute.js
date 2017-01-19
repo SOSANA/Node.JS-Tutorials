5f8699d181fe0c0bde7af9332854a75b5e8ab48a
sosana <thesosana@gmail.com>
2017-01-19 14:09:15 -0800

    adding delete route handler

--- mongo2016/muber/routes/driversRoute.js
+++ mongo2016/muber/routes/driversRoute.js
@@ -4,4 +4,5 @@ export default (app) => {
   app.get('/api', DriversController.greeting);
   app.post('/api/drivers', DriversController.create);
   app.put('/api/drivers/:id', DriversController.edit);
+  app.delete('/api/drivers/:id', DriversController.delete);
 };
