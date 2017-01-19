d109be1b5aa3f0941d87205743bf5e8bdfe0f473
sosana <thesosana@gmail.com>
2017-01-19 14:03:36 -0800

    adding new test for editing a driver

--- mongo2016/muber/test/controllers/drivers_controller_test.js
+++ mongo2016/muber/test/controllers/drivers_controller_test.js
@@ -19,4 +19,21 @@ describe('Drivers controller', () => {
         });
     });
   });
+
+  it('PUT to /api/drivers/:id edits an existing driver', (done) => {
+    const driver = new Driver({ email: 't@t.com', driving: false });
+
+    driver.save().then(() => {
+      request(app)
+        .put(`/api/drivers/${driver._id}`)
+        .send({ driving: true })
+        .end(() => {
+          Driver.findOne({ email: 't@t.com' })
+            .then((driverEdit) => {
+              assert(driverEdit.driving === true);
+              done();
+            });
+        });
+    });
+  });
 });
