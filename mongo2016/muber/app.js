9b408d2c94f5747117c8035713d9363fdc6b365f
sosana <thesosana@gmail.com>
2017-01-19 13:31:37 -0800

    adding error middleware

--- mongo2016/muber/app.js
+++ mongo2016/muber/app.js
@@ -15,6 +15,12 @@ app.use(bodyParser.json());
 
 routes(app);
 
+// applying error middleware
+app.use((err, req, res, next) => { // eslint-disable-line
+  // console.log(err); // eslint-disable-line
+  res.status(422).send({ error: err.message });
+});
+
 app.listen(3050, () => { // eslint-disable-line
   console.log('Running on port: 3050'); // eslint-disable-line
 });
