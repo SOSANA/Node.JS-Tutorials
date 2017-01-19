b67a0f83bdb1011ab9c55e0e6f67ac06140707f1
sosana <thesosana@gmail.com>
2017-01-19 13:47:43 -0800

    adding our edit handler route

--- mongo2016/muber/routes/driversRoute.js
+++ mongo2016/muber/routes/driversRoute.js
@@ -3,4 +3,5 @@ import DriversController from '../controllers';
 export default (app) => {
   app.get('/api', DriversController.greeting);
   app.post('/api/drivers', DriversController.create);
+  app.put('/api/drivers/:id', DriversController.edit);
 };
