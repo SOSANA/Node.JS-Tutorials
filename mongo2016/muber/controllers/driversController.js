c7ddce1b5af2d134f462e01dd3bbe9ca69602121
sosana <thesosana@gmail.com>
2017-01-19 13:47:14 -0800

    creating driver edit controller

--- mongo2016/muber/controllers/driversController.js
+++ mongo2016/muber/controllers/driversController.js
@@ -13,5 +13,15 @@ export default {
     Driver.create(driverProps)
       .then(driver => res.send(driver))
       .catch(next);
+  },
+
+  edit(req, res, next) {
+    const driverId = req.params.id;
+    const driverProps = req.body;
+
+    Driver.findByIdAndUpdate({ _id: driverId }, driverProps)
+      .then(() => Driver.findById({ _id: driverId }))
+      .then(driver => res.send(driver))
+      .catch(next);
   }
 };
