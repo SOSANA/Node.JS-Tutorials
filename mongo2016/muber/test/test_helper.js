78f3c5b6324c1ef660b0b584690ea3eadae6604b
sosana <thesosana@gmail.com>
2017-01-19 16:19:35 -0800

    updating to ensureIndex on geoNear

--- mongo2016/muber/test/test_helper.js
+++ mongo2016/muber/test/test_helper.js
@@ -12,6 +12,9 @@ before((done) => {
 beforeEach((done) => {
   const { drivers } = mongoose.connection.collections;
   drivers.drop()
+    // makes sures before any of the tests are ran that a index is in place
+    // over the 'geometry.coordinates' property on the drivers collection
+    .then(() => drivers.ensureIndex({ 'geometry.coordinates': '2dsphere' }))
     .then(() => done())
     .catch(() => done());
 });
