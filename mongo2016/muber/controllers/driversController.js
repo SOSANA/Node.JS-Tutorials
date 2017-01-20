cd085a5087b471654c03b5a29f87d78f8852f69b
sosana <thesosana@gmail.com>
2017-01-19 16:20:45 -0800

    adding new index function controller

--- mongo2016/muber/controllers/driversController.js
+++ mongo2016/muber/controllers/driversController.js
@@ -5,6 +5,18 @@ export default {
     res.send({ hi: 'there' });
   },
 
+  index(req, res, next) {
+    const { lng, lat } = req.query; // ex: http://google.com?lng=80&lat=20
+
+    Driver.geoNear(
+      // parseFloat() (native js function) pulls out the full decimal value
+      { type: 'Point', coordinates: [parseFloat(lng), parseFloat(lat)] },
+      { spherical: true, maxDistance: 200000 } // 200,000 meters or 200 kms
+    )
+      .then(drivers => res.send(drivers))
+      .catch(next);
+  },
+
   create(req, res, next) {
     // shows email of driver object we are trying to create
     // console.log(req.body); // eslint-disable-line
